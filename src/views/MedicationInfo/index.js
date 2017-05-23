7eb30940304dceb854d63eb5637fa2fdceb3e4b5
PiotrBerebecki <petebere@gmail.com>
2017-05-23 16:40:31 +0100

    Style medication info page
    relates #7 #21

--- src/views/MedicationInfo/index.js
+++ src/views/MedicationInfo/index.js
@@ -3,10 +3,15 @@ import styled from 'styled-components';
 
 import { StyledLink } from './../../styled';
 import { PageContainer } from './../../styled';
+import astronaut from './assets/astronaut.svg';
+
+const MedicationInfoWrapper = styled.div`
+  padding-left: 1rem;
+  padding-right: 1rem;
+  padding-top: 2rem;
+`;
 
 const PageTitle = styled.h1`
-  padding-top: 1rem;
-  background-color: white;
   color: #052136;
   font-weight: 700;
   text-align: center;
@@ -14,23 +19,33 @@ const PageTitle = styled.h1`
 `;
 
 const InfoList = styled.ul`
-  background-color: white;
-  color: #787878;
 `;
 
 const InfoItem = styled.li`
-  border-bottom: solid 1px #ccc;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: 2rem;
   padding: 1rem;
+  border-radius: 3px;
+  background-color: #fafafa;
+`;
+
+const SectionIcon = styled.img`
+  margin-bottom: 1rem;
+  width: 7rem;
+  height: auto;
 `;
 
 const SectionTitle = styled.h2`
   margin-bottom: 1rem;
   text-align: center;
+  color: #575757;
   font-weight: 700;
 `;
 
 const Description = styled.p`
-  color: #aaa;
+  color: #898989;
 `;
 
 class MedicationInfo extends Component {
@@ -40,41 +55,49 @@ class MedicationInfo extends Component {
         <StyledLink to="/">
           Home
         </StyledLink>
+        <MedicationInfoWrapper>
 
-        <PageTitle>
-          {this.props.match.params.medication}
-        </PageTitle>
+          <InfoList>
+            <InfoItem>
+              <PageTitle>
+                {this.props.match.params.medication}
+              </PageTitle>
+            </InfoItem>
 
-        <InfoList>
-          <InfoItem>
-            <SectionTitle>Effects</SectionTitle>
-            <Description>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </Description>
-          </InfoItem>
+            <InfoItem>
+              <SectionIcon src={astronaut} />
+              <SectionTitle>Effects</SectionTitle>
+              <Description>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </Description>
+            </InfoItem>
 
-          <InfoItem>
-            <SectionTitle>Effects</SectionTitle>
-            <Description>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </Description>
-          </InfoItem>
+            <InfoItem>
+              <SectionIcon src={astronaut} />
+              <SectionTitle>Effects</SectionTitle>
+              <Description>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </Description>
+            </InfoItem>
 
-          <InfoItem>
-            <SectionTitle>Effects</SectionTitle>
-            <Description>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </Description>
-          </InfoItem>
+            <InfoItem>
+              <SectionIcon src={astronaut} />
+              <SectionTitle>Effects</SectionTitle>
+              <Description>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </Description>
+            </InfoItem>
 
-          <InfoItem>
-            <SectionTitle>Effects</SectionTitle>
-            <Description>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </Description>
-          </InfoItem>
+            <InfoItem>
+              <SectionIcon src={astronaut} />
+              <SectionTitle>Effects</SectionTitle>
+              <Description>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </Description>
+            </InfoItem>
 
-        </InfoList>
+          </InfoList>
+        </MedicationInfoWrapper>
       </PageContainer>
     );
   }
