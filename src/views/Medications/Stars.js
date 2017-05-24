9f8df1e52b104baf957c663ced954bf31ed9fc41
Akin909 <akin.sowemimo@gmail.com>
2017-05-24 17:45:26 +0100

    add stars file

---
+++ src/views/Medications/Stars.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './Stars.css';
+
+const Stars = ({ drugs }) => (
+        //{data[med].drugs.map((drug, i) => (
+          //<StarContainer
+            //left={dimensions[i].left}
+            //top={dimensions[i].top}
+            //key={uuid()}
+          //>
+            //<Medication
+              //id={drug.name}
+              //onClick={e => this.handleClick(e, drug)}
+              //size={dimensions[i].size}
+            ///>
+            //<StarCaption> {drug.name} </StarCaption>
+          //</StarContainer>
+        //))}
+);
+
+Stars.defaultProps = {
+};
+
+Stars.propTypes = {
+};
+
+export default Stars;
