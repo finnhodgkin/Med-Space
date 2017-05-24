3e1967766d05318738a5e49be74fde7fde9cc376
Akin909 <akin.sowemimo@gmail.com>
2017-05-24 11:17:01 +0100

    Add actual information to database.

--- src/database.js
+++ src/database.js
@@ -1,31 +1,188 @@
 export default {
   OCD: {
     description: 'Difficulties in controlling obsessive thoughts and compulsive behaviours',
-    drugs: ['Citalopram', 'Sertraline', 'Fluoxetine'],
-  },
-  depression: {
-    description: 'Most young people experience ups and downs, especially during their teenage years, and will feel upset or down about certain things going on in their lives. But if sad, lonely or anxious feelings go on for longer periods of time to the extent they affect your everyday life, this could be depression',
-    drugs: [
-      'Citalopram',
-      'Sertraline',
-      'Fluoxetine',
-      'Amitryptaline',
-      'Lithium',
+    drugs: [{
+        name: 'Citalopram',
+        pronunciation: 'sit-AL-o-pram',
+        use: 'Citalopram is a Selective Serotonin Re-uptake Inhibitor. It helps make more serotonin available in your brain.',
+        'side-effects': [
+          'Difficulty sleeping (insomnia) - try taking your dose first thing in the morning',
+          'Dry mouth (a dry mouth increases the risk of tooth decay, so do clean your teeth more often than usual and avoid drinking sugary drinks)',
+          'Feeling sick (nausea) or vomiting - try taking your citalopram with or just after food. This tends to wear off after a few days.',
+          'Lower sex drive, or (for men) problems with ejaculation and erection and (for women) failing to reach an orgasm - talk to your doctor or pharmcist if you get this. This can be a symptom of depression.',
+        ],
+      },
+      {
+        name: 'Sertraline',
+        pronunciation: 'SIR-tra-leen',
+        use: 'A Selective Serotonin Re-uptake Inhibitor (SSRI) helps make more serotonin available in your brain.',
+        'side-effects': [
+          'thoughts of harming yourself or taking your own life.',
+          'any combination of the following symptoms - high fever, agitation, confusion, trembling, sweating, diarrhoea (loose poo), fast heartbeat or weird movements of your muscles – which could be ‘Serotonin Syndrome’',
+          'a severe skin rash that causes blistering, including your mouth and tongue. These may be signs of a condition known as Stevens Johnson Syndrome, or Toxic Epidermal Necrolysis (TEN). Your doctor will stop your treatment in these cases.',
+          'allergic reaction or allergy, which may include symptoms such as an itchy skin rash, breathing problems, wheezing, swollen eyelids, face or lips',
+          'if your skin or the whites of your eyes go yellow, which may mean liver damage (jaundice)',
+          'if you feel restless and are not able to sit or stand still after you start to take sertraline',
+          'if you have a fit (seizure)',
+          'if you feel overexcited or very happy, as this may be a manic episode',
+          'repeated movements of your face and tongue that you cannot control',
+          'for men, a painful erection that lasts for a long time (priapism)',
+        ],
+      },
+      {
+        name: 'Fluoxetine',
+        pronunciation: 'Flu-OX-et-een',
+        use: 'Fluoxetine is a type of antidepressant called a Selective Serotonin Re-uptake Inhibitor. It can be used to help with low mood and anxiety.',
+        'side-effects': [
+          'Difficulty sleeping (insomnia)',
+          'headache and feeling tired',
+          'loose poo (diarrhoea)',
+          'feeling sick (nausea)',
+        ]
+      }
     ],
   },
-  ADHD: {
-    description: 'Difficulties with concentration, impulsivity and activity levels',
-    drugs: [
-      'Atomoxetine (Strattera)',
-      'Methylphenidate (Concerta, Medikinet, Equasy, Ritalin)',
-    ],
-  },
-  anxiety: {
-    description: 'Difficulties with overwhelming worry in different situations',
-    drugs: ['Chlorpromazine', 'Diazepam'],
-  },
-  psychosis: {
-    description: 'Difficulties with perception or interpretation of events',
-    drugs: ['Chlorpromazine', 'Olanzapine', 'Quetiapine', 'Aripiprazole']
-  }
-};
+    depression: {
+      description: 'Most young people experience ups and downs, especially during their teenage years, and will feel upset or down about certain things going on in their lives. But if sad, lonely or anxious feelings go on for longer periods of time to the extent they affect your everyday life, this could be depression',
+      drugs: [{
+          name: 'Citalopram',
+          pronunciation: 'sit-AL-o-pram',
+          use: 'Citalopram is a Selective Serotonin Re-uptake Inhibitor. It helps make more serotonin available in your brain.',
+          'side-effects': [
+            'Difficulty sleeping (insomnia) - try taking your dose first thing in the morning',
+            'Dry mouth (a dry mouth increases the risk of tooth decay, so do clean your teeth more often than usual and avoid drinking sugary drinks)',
+            'Feeling sick (nausea) or vomiting - try taking your citalopram with or just after food. This tends to wear off after a few days.',
+            'Lower sex drive, or (for men) problems with ejaculation and erection and (for women) failing to reach an orgasm - talk to your doctor or pharmcist if you get this. This can be a symptom of depression.',
+          ],
+        },
+        {
+          name: 'Sertraline',
+          pronunciation: 'SIR-tra-leen',
+          use: 'A Selective Serotonin Re-uptake Inhibitor (SSRI) helps make more serotonin available in your brain.',
+          'side-effects': [
+            'thoughts of harming yourself or taking your own life.',
+            'any combination of the following symptoms - high fever, agitation, confusion, trembling, sweating, diarrhoea (loose poo), fast heartbeat or weird movements of your muscles – which could be ‘Serotonin Syndrome’',
+            'a severe skin rash that causes blistering, including your mouth and tongue. These may be signs of a condition known as Stevens Johnson Syndrome, or Toxic Epidermal Necrolysis (TEN). Your doctor will stop your treatment in these cases.',
+            'allergic reaction or allergy, which may include symptoms such as an itchy skin rash, breathing problems, wheezing, swollen eyelids, face or lips',
+            'if your skin or the whites of your eyes go yellow, which may mean liver damage (jaundice)',
+            'if you feel restless and are not able to sit or stand still after you start to take sertraline',
+            'if you have a fit (seizure)',
+            'if you feel overexcited or very happy, as this may be a manic episode',
+            'repeated movements of your face and tongue that you cannot control',
+            'for men, a painful erection that lasts for a long time (priapism)',
+          ],
+        },
+        {
+          name: 'Fluoxetine',
+          pronunciation: 'Flu-OX-et-een',
+          use: 'Fluoxetine is a type of antidepressant called a Selective Serotonin Re-uptake Inhibitor. It can be used to help with low mood and anxiety.',
+          'side-effects': [
+            'Difficulty sleeping (insomnia)',
+            'headache and feeling tired',
+            'loose poo (diarrhoea)',
+            'feeling sick (nausea)',
+          ],
+        },
+        {
+          name: 'Amitryptaline',
+          pronunciation: 'Ammy-TRIP-ti-leen',
+          use: 'Amitriptyline is a tricyclic antidepressant medicine. A tricyclic antidepressant medicine helps to lift your mood. They are used for other symptoms, however, including bedwetting and to help with nerve pain.',
+          'side-effects': [
+            'feeling sleepy',
+            'dry mouth',
+            'blurred eyesight, changes to your eyesight, or increased pressure in the eye',
+            'constipation',
+            'fever',
+            'difficulty in going to pee'
+          ]
+        },
+        {
+          name: 'Lithium',
+          pronunciation: 'LITH-ee-um',
+          use: 'Lithium can be used to help a range of mental health conditions. Lithium helps to keep your mood from becoming too low or too high, which is why it is classed as a mood stabiliser medicine',
+          'side-effects': [
+            'Feeling unusually sleepy',
+            'Severe hand shake (‘tremor’)',
+            'Stomach ache along with feeling sick and having diarrhoea',
+            'Muscle weakness',
+            'Being unsteady on your feet',
+            'Muscle twitches',
+            'Slurring of words – so that it is difficult for others to understand what you are saying',
+            'Blurred vision',
+            'Confusion'
+          ]
+        },
+      ],
+    },
+    ADHD: {
+      description: 'Difficulties with concentration, impulsivity and activity levels',
+      drugs: [{
+          name: 'Atomoxetine (Strattera)',
+          pronunciation: 'A-tom-OX-et-een',
+          use: 'Atomoxetine is not a central nervous stimulant, which makes it different from other treatments for Attention Deficit Hyperactivity Disorder ADHD.',
+          'side-effects': [
+            'feeling or having a very fast heartbeat, or an uneven heartbeat',
+            'thinking about or feeling like killing yourself',
+            'feeling aggressive, unfriendly or very angry',
+            'having mood swings or mood changes',
+            'having a serious allergic reaction with symptoms of swelling of the face and throat',
+            'difficulty breathing',
+            'hives (small raised, itchy patches of skin)',
+            'fits (seizures)',
+            'psychotic symptoms including hallucinations (hearing voices or seeing things which are not there), or believing things that are not true, or being suspicious',
+            'in men, having a painful and long-lasting erection (priapism)'
+          ]
+        },
+        {
+          name: 'Methylphenidate (Concerta, Medikinet, Equasy, Ritalin)',
+          pronunciation: 'MEE-thile-FEN-i-date',
+          use: 'Methylphenidate is a Central Nervous Stimulant. You can take methylphenidate as tablets or capsules.',
+          'side-effects': [
+            'headache – try paracetamol or talk to your pharmacist about options to help',
+            'feeling nervous',
+            'not being able to sleep'
+          ]
+        },
+      ],
+    },
+    anxiety: {
+      description: 'Difficulties with overwhelming worry in different situations',
+      drugs: [{
+        name: 'Chlorpromazine',
+        pronunciation: 'klor-PRO-ma-zeen',
+        use: 'An antipsychotic medicine helps to adjust the levels of dopamine and other chemicals available in your brain to help with the symptoms.',
+        'side-effects': [
+          'Difficulty in swallowing, or any other symptoms like swelling in the mouth, tongue, face and throat, itching, rash, that could be an allergic reaction to the medicine',
+          'Muscles going stiff or rigid with high fever, sweating, looking pale, feeling strange and fainting or losing consciousness, or very rapid or irregular heartbeat (tachycardia) - this may be a serious and life-threatening side-effect called neuroleptic malignant syndrome',
+          'If a man gets a painful erection that lasts for a long time',
+          'Skin reactions - rashes, flaking of the skin or red blisters on the skin',
+          'The whites of your eyes and skin become yellow in colour - this could be jaundice - look out for this especially in the first month of taking the medicine'
+        ]
+      }, {
+        name: 'Diazepam',
+        pronunciation: 'Die-A-zi-pam',
+        use: 'Diazepam improves the effect of a chemical in the brain called gamma-aminobutyric acid (GABA). GABA is a chemical that blocks transmission across nerves in the brain and has a ‘calming effect’',
+        'side-effects': [
+          'extreme sleepiness that interferes with your daily life',
+          'slurred speech',
+          'headache', 'light-headedness, a ‘spinning’ sensation, unsteadiness or clumsiness and loss of co-ordination (you may notice these even after a single dose and this may continue into the following day)',
+          'confusion', 'or memory loss (which may be experienced several hours after taking diazepam)',
+          'seeing or sensing things that are not there', 'unusual behaviour', 'difficulty concentrating', 'agitation/irritability', 'getting overexcited or restless', 'feeling very angry (rage), numbed emotions, or depression with thoughts of hurting or killing yourself'
+        ]
+      }],
+    },
+    psychosis: {
+      description: 'Difficulties with perception or interpretation of events',
+      drugs: [{
+        name: 'Chlorpromazine',
+        pronunciation: 'klor-PRO-ma-zeen',
+        use: 'An antipsychotic medicine helps to adjust the levels of dopamine and other chemicals available in your brain to help with the symptoms.',
+        'side-effects': [
+          'Difficulty in swallowing, or any other symptoms like swelling in the mouth, tongue, face and throat, itching, rash, that could be an allergic reaction to the medicine',
+          'Muscles going stiff or rigid with high fever, sweating, looking pale, feeling strange and fainting or losing consciousness, or very rapid or irregular heartbeat (tachycardia) - this may be a serious and life-threatening side-effect called neuroleptic malignant syndrome',
+          'If a man gets a painful erection that lasts for a long time',
+          'Skin reactions - rashes, flaking of the skin or red blisters on the skin',
+          'The whites of your eyes and skin become yellow in colour - this could be jaundice - look out for this especially in the first month of taking the medicine'
+      ]}]},
+  };
+  //, 'Olanzapine', 'Quetiapine', 'Aripiprazole'],
